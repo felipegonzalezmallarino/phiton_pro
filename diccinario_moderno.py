25b2a6daff5ca21f34167e19dd3be8de074d1f78
felipegonzalezmallarino <felipe.gonzalez.mallarino@gmail.com>
2026-09-07 19:24:01 -0300

    Create diccinario_moderno.py

---
+++ diccinario_moderno.py
@@ -0,0 +1,15 @@
+meme_dict = {
+            "cringe": "Algo excepcionalmente raro o embarazoso",
+            "lol": "Una respuesta común a algo gracioso",
+            "aura":"Prestigio, carisma o presencia que impone una persona",
+            "´prime":"El mejor momento o la etapa de máximo rendimiento de alguien",
+            "de chill": "Estar relajado o sin preocupaciones"
+            "sheesh": "ligera desaprobación"
+            "creppy": "aterrador, siniestro"
+            "aggro":  "ponerse agresivo/enojado"
+}
+word = input("Escribe una palabra que no entiendas (estas: cringe, lol, aura, prime o de chill) (¡sin mayúsculas!): ")
+if word in meme_dict.keys():
+    print("conosemos esa palabra significa",meme_dict.)
+else:
+    print("no conosemos es palabra nueva")
